ce65f9b8d23897fcccf585658f544feb294e0b2e
Delta-44 <dalonso306@gmail.com>
2025-05-27 09:21:39 +0200

    ???''''''

--- src/main/java/com/me/sensor/controllers/BattleViewController.java
+++ src/main/java/com/me/sensor/controllers/BattleViewController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.List;
 
 @Controller
@@ -20,7 +21,7 @@ public class BattleViewController {
 
     @GetMapping("/battle-view")
     public String showBattle(Model model) {
-        // Genera dos equipos de 3 héroes cada uno
+        // Genera dos equipos aleatorios, cada uno con 3 héroes
         List<Superhero> teamA = teamGeneratorService.getRandomTeam(3);
         List<Superhero> teamB = teamGeneratorService.getRandomTeam(3);
         String winner = battleService.determineWinner(teamA, teamB);
@@ -29,6 +30,6 @@ public class BattleViewController {
         model.addAttribute("teamB", teamB);
         model.addAttribute("winner", winner);
 
-        return "battle";  // Se buscará el template battle.html
+        return "battle"; // Esto buscará el template battle.html
     }
 }
